1bc4e739e0e652f242ddcb2e4ec17f7e6dea8c95
Douglas Eleutério <douglaseleuterio@lett.digital>
2023-12-23 18:39:43 -0300

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div id="map" style="width: 1200px; height: 1200px" class="map"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      map: null,
+      marker: null
+    }
+  },
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      const location = { lat: -34.397, lng: 150.644 };
+      this.map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: location,
+      });
+      this.marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+      });
+    }
+  }
+}
+</script>
+
+<style>
+.map {
+  top: 0;
+  left: 0;
+}
+</style>
