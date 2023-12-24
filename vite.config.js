a4b489ade3a5edd0bc42d8917ae14a4fc5f054f5
Douglas Eleutério <douglaseleuterio@lett.digital>
2023-12-24 12:50:03 -0300

    iniciando parser de md

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import notionToMdPlugin from './plugins/notionToMdPlugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // notionToMdPlugin(),
   ],
   resolve: {
     alias: {
